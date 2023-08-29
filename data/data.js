bc26cda9a0da9bcc4f66a813119d88d7c84d01c4
Niki-q <petrychenko.nikita.learn@gmail.com>
2023-08-29 15:19:14 +0300

    add view table for notes. Add archive table and archive buttons implementation

--- data/data.js
+++ data/data.js
@@ -1,35 +1,115 @@
-export class Note {
-    constructor(name, content, category, archived) {
-        this.name = name
-        this.created = new Date()
-        this.content = content
-        this.category = category
-        this.archived = archived
-        this.dates = get_dates(content)
-    }
-}
+//  Constants
 
- const get_dates = function (content) {
-    // Regular expression to match dates in the format "3/5/2021"
-    const datePattern = /\d{1,2}\/\d{1,2}\/\d{4}/g;
+const  svgFolderPath  =  './data/svg/'
 
-    // Use the RegExp.exec() method to find all matches in the text
-    const matches = [];
-    let match;
-
-    while ((match = datePattern.exec(content)) !== null) {
-        matches.push(match[0]);
-    }
-
-    return matches;
-}
-
-export const Data = [
+const defaultNotesList = [
     new Note('Shoppingc list', 'Need to buy groceries and household essentials on 5/5/2021.', 'Task',false),
     new Note('The theory of evolution','Planning to read about Darwin\'s theory of natural selection tomorrow, on 5/6/2021.','Random Thought',false),
     new Note('New Feature','Implementing the \'undo\' feature in the app today, on 5/7/2021.','Idea',false),
     new Note('William Gaddis','Starting to read \'The Recognitions\' by William Gaddis on 5/8/2021.','Quote',false),
     new Note('Books','Returning borrowed books to the library on 5/9/2021.','Task',false),
-    new Note('a1','','',true),
-    new Note('a2', '','',true)
+    new Note('Dentist','I’m gonna have a dentist appointment on the 3/5/2021, I moved it from 5/5/2021','Task',true),
+    new Note('a2', '','Quote',true)
 ]
+
+
+// Classes
+
+export class Note {
+    constructor(name, content, category, archived) {
+        this.id = generateUniqueId()
+        this.name = name
+        this.created = formatDateToMonthDDYYYY(new Date())
+        this.content = content
+        this.category = category
+        this.isArchived = archived
+
+        const datePattern = /\d{1,2}\/\d{1,2}\/\d{4}/g;
+
+        const matches = [];
+        let match;
+
+        while ((match = datePattern.exec(content)) !== null) {
+            matches.push(match[0]);
+        }
+
+        this.dates = matches
+    }
+    getCategoryIconPath(){
+        return getIconPath(this.category)
+    }
+    toTableTr(tr){
+        const logoCell = document.createElement("td");
+        const iconContainer = document.createElement("div")
+        iconContainer.className = 'IconContainer'
+        const logoIcon = document.createElement("img");
+        logoIcon.src = this.getCategoryIconPath()
+        logoIcon.className = 'CategoryIcon'
+
+        const tdName = document.createElement("td");
+        tdName.textContent = this.name
+        const tdCreated = document.createElement("td");
+        tdCreated.textContent = this.created
+        const tdCategory = document.createElement("td");
+        tdCategory.textContent = this.category
+        const tdContent = document.createElement("td");
+        tdContent.textContent = this.content
+        const tdDates = document.createElement("td");
+        tdContent.tdDates = this.dates.join(', ')
+        iconContainer.appendChild(logoIcon);
+        logoCell.appendChild(iconContainer);
+
+        tr.append(logoCell, tdName, tdCreated, tdCategory, tdContent, tdDates)
+    }
+}
+
+class NoteStorage{
+    constructor(list) {
+        this.actual = list.filter(note => !note.isArchived)
+        this.archived = list.filter(note => note.isArchived)
+
+    }
+    getAllNotes(){
+        return this.actual.concat(this.archived)
+    }
+    getById(id){
+        return this.getAllNotes().find(note => note.id===id)
+    }
+    archiveNote(id){
+        const mustBeArchiveNote = this.getById(id)
+
+        this.actual = this.actual.filter(note => note !== mustBeArchiveNote)
+        this.archived.push(mustBeArchiveNote)
+        mustBeArchiveNote.isArchived = true
+    }
+    unArchiveNote(id){
+        const mustBeUnArchiveNote = this.getById(id)
+
+
+        this.archived = this.archived.filter(note => note !== mustBeUnArchiveNote)
+        this.actual.push(mustBeUnArchiveNote)
+        mustBeUnArchiveNote.isArchived = false
+    }
+}
+
+
+function generateUniqueId() {
+    const timestamp = Date.now().toString(36);
+    const randomPart = Math.random().toString(36).substr(2, 5);
+
+    return timestamp + randomPart;
+}
+
+function formatDateToMonthDDYYYY(date) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+}
+
+
+// Export variables
+
+export const getIconPath = function (name){
+    return `${svgFolderPath}${name}.svg`
+}
+
+export const Data = new NoteStorage(defaultNotesList)
--- index.js
+++ index.js
@@ -1,42 +1,17 @@
-import {archiveNote, Data, getIconPath} from './data/data.js'
+import {Data, getIconPath} from './data/data.js'
 
-console.log('index.js')
-const fillTD = function (text){
-    let td = document.createElement("td");
-    td.textContent = text
-    return td
-}
-// Get the container element by its ID
 const notesTable = document.getElementById("notesTable");
 
 const fillTable = function (data, name){
-    console.log('fill table')
     const thead = document.getElementById('thead')
     thead.textContent = name
     data.forEach(note => {
         const trNote = document.createElement("tr");
-        // trNote.className = 'table-primary'
-        const logoCell = document.createElement("td");
-        const iconContainer = document.createElement("div")
-        iconContainer.className = 'IconContainer'
-        const logoIcon = document.createElement("img");
-        logoIcon.src = note.getCategoryIconPath()
-        logoIcon.className = 'CategoryIcon'
 
-        iconContainer.appendChild(logoIcon);
-        logoCell.appendChild(iconContainer);
-        trNote.appendChild(logoCell);
-
-        trNote.appendChild(fillTD(note.name))
-        trNote.appendChild(fillTD(note.created))
-        trNote.appendChild(fillTD(note.category))
-        trNote.appendChild(fillTD(note.content))
-        trNote.appendChild(fillTD(note.dates.join(', ')))
+        note.toTableTr(trNote)
 
         const editButtonCell = document.createElement("td");
         const editButton = document.createElement("img");
-        // editButton.className = "btn btn-info"; // Применяем классы Bootstrap
-        // editButton.textContent = "Edit";
         editButton.src = getIconPath('Edit')
         editButton.className = 'ToolIcon'
         editButtonCell.appendChild(editButton);
@@ -44,25 +19,27 @@ const fillTable = function (data, name){
 
         const archiveButtonCell = document.createElement("td");
         const archiveButton = document.createElement("img");
-        // archiveButton.className = "btn btn-warning"; // Применяем классы Bootstrap
-        // archiveButton.textContent = "Archive";
-        archiveButton.src = getIconPath('Archive')
+        const ArchiveViewButton = document.getElementById('ArchiveSwitch')
+        if (ArchiveView){
+            archiveButton.src = getIconPath('Archive Up')
+            ArchiveViewButton.src =getIconPath('Archive Up White')
+        }
+        else {
+            archiveButton.src = getIconPath('Archive Down')
+            ArchiveViewButton.src =getIconPath('Archive Down White')
+        }
         archiveButton.className = 'ToolIcon ArchiveButton'
         archiveButtonCell.appendChild(archiveButton);
         trNote.appendChild(archiveButtonCell);
 
         const deleteButtonCell = document.createElement("td");
         const deleteButton = document.createElement("img");
-        // deleteButton.className = "btn btn-danger"; // Применяем классы Bootstrap
-        // deleteButton.textContent = "Delete";
         deleteButton.src = getIconPath('Delete')
         deleteButton.className = 'ToolIcon'
         deleteButtonCell.appendChild(deleteButton);
         trNote.appendChild(deleteButtonCell);
 
 
-
-        // listItem.textContent = `Name: ${obj.name}, Content: ${obj.content}, Dates: ${obj.dates}`;
         trNote.id = note.id
         notesTable.appendChild(trNote);
     });
@@ -70,54 +47,45 @@ const fillTable = function (data, name){
 }
 
 const clearTable = function (){
-    console.log('clear table')
-// Проверить, существует ли такой элемент
     if (notesTable) {
-        // Получить первый вложенный элемент
-        const firstChild = notesTable.firstElementChild;
-
-        // Удалить все остальные вложенные элементы
         while (notesTable.childElementCount > 1) {
             notesTable.removeChild(notesTable.lastElementChild);
         }
     }
 }
-document.addEventListener('DOMContentLoaded',()=>{
-    refreshTable()
-})
-
-// const createButtonDiv = document.getElementById('createButton')
-// const createBUtton = createButtonDiv.firstChild
 
 const refreshTable = function (){
     clearTable()
     if (ArchiveView){
-        fillTable(Data.filter((note) => {
-            return note.archived
-        }),'Archive Notes')
+        fillTable(Data.archived,'Archive Notes')
     }
     else {
-        fillTable(Data.filter((note) => {
-            return !note.archived
-        }),'Actual Notes')
+        fillTable(Data.actual,'Actual Notes')
     }
+
+    // refresh listeners to buttons
 
     const ArchiveButtons = document.getElementsByClassName('ArchiveButton')
 
     for (const button of ArchiveButtons) {
         button.addEventListener('click', function() {
-            // Получаем ID родителя (предполагается, что родитель — это tr элемент)
-            const parentElement = this.closest('tr'); // Ищем ближайший родительский tr элемент
+            const parentElement = this.closest('tr');
             if (parentElement) {
                 const parentId = parentElement.id;
-                archiveNote(parentId);
-                // console.log(`Note with ID ${parentId} has been archived`);
+                if (Data.getById(parentId).isArchived)
+                    Data.unArchiveNote(parentId)
+                else
+                    Data.archiveNote(parentId)
                 refreshTable()
             }
         });
     }
 
 }
+
+
+
+// Listeners
 
 const ArchiveSwitch = document.getElementById('ArchiveSwitch')
 
@@ -128,3 +96,6 @@ ArchiveSwitch.addEventListener('click',() =>{
 } )
 
 
+document.addEventListener('DOMContentLoaded',()=>{
+    refreshTable()
+})
